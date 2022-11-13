45e845ae29318012f080e05b2eb1655d1afd19ef
Matthew Lin <matthew.lin.earth@gmail.com>
2022-11-13 15:58:36 -0600

    concept proof

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reno/poc/ConceptOpenCVSignalSleeveDetection.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.Reno.poc;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+import org.firstinspires.ftc.teamcode.Reno.HardwareRobot;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous(name="POC - OpenCV Signal Sleeve", group ="Concept")
+public class ConceptOpenCVSignalSleeveDetection extends LinearOpMode {
+
+    ConceptSleeveDetection sleeveDetection;
+    OpenCvCamera camera;
+    
+    // Name of the Webcam to be set in the config
+    String webcamName = "Webcam 1";
+
+    private HardwareRobot robot   = new HardwareRobot();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
+        sleeveDetection = new ConceptSleeveDetection();
+        camera.setPipeline(sleeveDetection);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(320,240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+            }
+
+            @Override
+            public void onError(int errorCode) {}
+        });
+
+        while (!isStarted()) {
+            telemetry.addData("ROTATION: ", sleeveDetection.getPosition());
+            telemetry.update();
+        }
+
+        waitForStart();
+    }
+}
